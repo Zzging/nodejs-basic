98fbf090e3e4f14485bb1335fa6174abcd25ea23
Zzging <47513118+Zzging@users.noreply.github.com>
2019-09-21 12:24:15 +0700

    add in read me

---
+++ 2-if-for.js
@@ -0,0 +1,41 @@
+// const a = "a";
+// if (a === "a") {
+//   console.log("it's a");
+// } else {
+//   console.log("it's  not a");
+// }
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// console.log(i);
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+// let sum = 0;
+// for (let i = 1; i <= 1000; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+
+let a = 1; // int a = 1;
+let b = 1.1; // float b = 1.1;
+let c = "string"; // string c = "hello"
+let d = true; // bool d = true
+let e = false; // bool e = false
+let f = ["a", "b", "c"]; // string *f = ["a", "b", "c"]
+f[0] = "f";
+f.push("h");
+console.log(f);
+let h = [];
+let student = {
+  id: "61115267",
+  "name-lastname": "chanikan",
+  faculty: "potjamanpong"
+};
+console.log(student.name);
+student.name = "chanikan";
+console.log(student["name"]);
---
+++ 4-class.js
@@ -0,0 +1,20 @@
+class calculator {
+  static sum(x, y) {
+    return x, y;
+  }
+
+  sum2(x, y) {
+    return x + y;
+  }
+
+  multiply(x, y) {
+    return x * y;
+  }
+}
+
+const calculatororodject = new calculator();
+
+console.log(calculator.sum(1, 2));
+console.log(calculatororodject.sum2(2, 3));
+// console.log(calculatororodject).sum(5, 6);
+console.log(calculatororodject.multiply(10, 20));
